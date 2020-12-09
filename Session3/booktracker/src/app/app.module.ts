d527253bdc4a788f7327b464d1d536bbf61058e7
Brice Wilson <brice@5wilsons.net>
2020-12-09 11:41:26 -0500

    declared components

--- Session3/booktracker/src/app/app.module.ts
+++ Session3/booktracker/src/app/app.module.ts
@@ -8,11 +8,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { BooksModule } from './books/books.module';
 import { ReadersModule } from './readers/readers.module';
+import { LibraryComponent } from './library/library.component';
+import { InventoryComponent } from './inventory/inventory.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    DashboardComponent,
+    LibraryComponent,
+    InventoryComponent
   ],
   imports: [
     BrowserModule,
